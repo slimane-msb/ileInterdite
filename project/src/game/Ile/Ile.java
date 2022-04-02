4de2d2f101d91df5011c07ac6f282f328c95320a
slimane-IA <72779962+slimane-IA@users.noreply.github.com>
2022-04-02 15:37:23 +0200

    construction of the island with submerged borders

--- project/src/game/Ile/Ile.java
+++ project/src/game/Ile/Ile.java
@@ -1,13 +1,30 @@
 package game.Ile;
 
+import game.Utils.Level;
+
 import java.util.ArrayList;
 
 public class Ile {
-    private ArrayList<Zone> grille;
+    private int length;
+    private Zone[][] plateau;
     private boolean submerged;
 
-    public Ile( boolean submerged) {
-        this.grille = new ArrayList<Zone>() ;
+    public Ile( int length, boolean submerged) {
+        this.length=length;
+        // construction of the island with submerged zones in the borders
+        Level state;
+        Zone left,right,up,down;
+        for (int i=0;i<length;i++){
+            for (int j=0;j<length;j++){
+                state = ( i==0 || j==0 || i==length-1|| j==length-1)?Level.submerged:Level.dry; // is in the borders
+                up=(i==0)?null:plateau[i-1][j];
+                down=(i==length-1)?null:plateau[i+1][j];
+                left=(j==0)?null:plateau[i][j-1];
+                right=(j==length-1)?null:plateau[i][j+1];
+                this.plateau[i][j]=new Zone(state,null,null,this,left,right,up,down);
+            }
+        }
+
         this.submerged = submerged;
     }
 
