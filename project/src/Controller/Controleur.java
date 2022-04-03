7e5105076ae9a58ff9467790c7e4955335824bc0
slimane-IA <72779962+slimane-IA@users.noreply.github.com>
2022-04-03 12:14:21 +0200

    MVC -controleur-

--- project/src/Controller/Controleur.java
+++ project/src/Controller/Controleur.java
@@ -1,11 +1,33 @@
 package Controller;
 
 import Model.Island.Island;
+import View.Command.SearchKey;
+import View.Command.ToDry;
 import View.ViewIsland;
+import View.Window;
 
 public class Controleur {
+
+    private int length;
+    private Window window;
     private Island island;
-    private ViewIsland vueIsland;
+    private SearchKey searchKey;
+    private ToDry toDry;
+
+    public Controleur( Island island) {
+        this.length=island.getLength();
+        this.window = new Window(island.getLength() + "ile interdite");
+        this.island = island;
+        this.searchKey = new SearchKey();
+        this.toDry = new ToDry();
+        window.ajouteElement(new ViewIsland(island.getLength()));
+//        window.ajouteElement(searchKey);
+//        window.ajouteElement(toDry);
+        window.dessineFenetre();
+    }
+
+
+
 }
 
 
--- project/src/main/Main.java
+++ project/src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import Controller.Controleur;
 import Model.Island.Island;
 import View.*;
 import View.Command.SearchKey;
@@ -7,15 +8,8 @@ import View.Command.ToDry;
 
 public class Main {
     public static void main(String[] args) {
-        int nb = 8;
-        Window window = new Window(nb + "ile interdite");
-        Island island = new Island(nb);
-        SearchKey searchKey = new SearchKey();
-        ToDry indice = new ToDry();
-        window.ajouteElement(new ViewIsland(island.getLength()));
-        //fenetre.ajouteElement(searchKey);
-       // fenetre.ajouteElement(indice);
-        window.dessineFenetre();
+        Island island = new Island(10);
+        Controleur controleur = new Controleur(island);
     }
 }
 
