6232ad291c50879977bd121f0e79b7153787c7c1
Emilie <snapmili@live.fr>
2022-04-18 01:38:56 +0200

    update related to class island constructeur changes

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -5,7 +5,7 @@ import Model.Island.Island;
 
 public class Main {
     public static void main(String[] args) {
-        Island island = new Island(10);
+        Island island = new Island(10, (float) 0.4);
         Controller controller = new Controller(island);
     }
 }
